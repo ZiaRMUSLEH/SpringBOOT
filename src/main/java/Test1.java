028195b8a67f3bf720a8553ca96a2f5d353a3e60
ZiaRMUSLEH <ziarahmanmusleh@gmail.com>
2023-07-19 16:18:09 +0300

    Update Test1.java

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -5,5 +5,17 @@ public class Test1 {
         public void print(){
             System.out.println("message");
         }
+
+          public void print(){
+            System.out.println("message");
+        }
+
+
+    @DeleteMapping("/deleteById")
+    public ResponseEntity<String> deleteById(@RequestParam("id")Long id ){
+        service.deleteById(id);
+
+        return ResponseEntity.ok("Student deleted Successfully");
+    }
     }
 
